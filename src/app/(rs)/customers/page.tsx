7681c315b016104571d9e57e180e914644d6fa5e
Marco Soto Maceda <158858927+marcosotomac@users.noreply.github.com>
2025-08-13 01:03:49 -0500

    Added: simple routing

---
+++ src/app/(rs)/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Customers(){
+    return (
+        <h2>Customers page</h2>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Link } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-black bg-home-img bg-cover bg-center">
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
-        <div>
-          <h1>Welcome to Our Repair Shop</h1>
+        <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
+          <h1 className="text-4xl font-bold">Welcome to Our Repair Shop</h1>
           <address>
             <p>123 Repair St.</p>
             <p>Repair City, RC 12345</p>
