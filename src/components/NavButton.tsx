ac198fa6fba9fdb559b9b858bde9bf938645cc6d
Marco Soto Maceda <158858927+marcosotomac@users.noreply.github.com>
2025-08-13 01:13:52 -0500

    Added: Nav button

---
+++ src/components/NavButton.tsx
@@ -0,0 +1,24 @@
+import { LucideIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+type props = {
+  icon: LucideIcon;
+  label: string;
+  href?: string;
+};
+
+export function NavButton({ icon: Icon, label, href }: props) {
+  return (
+    <Button
+      variant="ghost"
+      size={"icon"}
+      aria-label={label}
+      title={label}
+      className="rounded-full"
+      asChild
+    >
+      {href ? <Link href={href}>{label}</Link> : <Icon />}
+    </Button>
+  );
+}
